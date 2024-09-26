4b3cf48fc619167bdc07992ed2340c51af84283a
Florian Erbs <florian@f-erbs.de>
2024-09-26 18:54:10 +0200

    use conf

--- HTTP/Server.go
+++ HTTP/Server.go
@@ -1,25 +1,39 @@
 package HTTP
 
 import (
+	"crypto/subtle"
+	"dankey/Config"
 	"dankey/DTO"
 	"dankey/Storage"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
 )
 
 type Server struct {
 	Provider Storage.Provider
 	Echo     *echo.Echo
+	conf     *Config.Config
 }
 
-func NewServer(provider Storage.Provider) *Server {
+func NewServer(provider Storage.Provider, config *Config.Config) *Server {
 	return &Server{
 		Provider: provider,
 		Echo:     echo.New(),
+		conf:     config,
 	}
 }
 
 func (s *Server) Start() {
+	s.Echo.Use(middleware.BasicAuth(func(username string, password string, c echo.Context) (bool, error) {
+		// Be careful to use constant time comparison to prevent timing attacks
+		if subtle.ConstantTimeCompare([]byte(username), []byte(s.conf.Username)) == 1 &&
+			subtle.ConstantTimeCompare([]byte(password), []byte(s.conf.Password)) == 1 {
+			return true, nil
+		}
+		return false, nil
+	}))
+
 	s.Echo.PUT("/put", s.Put)
 	s.Echo.GET("/get", s.Get)
 	//e.DELETE("/delete", s.Delete)
