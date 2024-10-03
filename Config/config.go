bc8d6d66a2e5fae73ae51c761da564cdc1b53d1a
Florian Erbs <florian@f-erbs.de>
2024-10-03 12:51:32 +0200

    Add port field to Config struct and handle default config settings.
    - Added a 'Port' field to the Config struct.
    - Refactored NewConfigWithPath function to use getDefaultConfig for default settings handling.

--- Config/config.go
+++ Config/config.go
@@ -9,6 +9,7 @@ import (
 type Config struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+	Port     string `json:"port"`
 }
 
 func NewConfig() (*Config, error) {
@@ -16,19 +17,38 @@ func NewConfig() (*Config, error) {
 }
 
 func NewConfigWithPath(filePath string) (*Config, error) {
-	config := Config{}
+	config := getDefaultConfig()
 	file, err := os.OpenFile(filePath, os.O_RDONLY, 0666)
 	if err != nil {
+		if os.IsNotExist(err) {
+			log.Warn().Msg("Config file not found. Using default config.")
+			log.Warn().Msg("Please create a config.json file in the root directory to customize the configuration.")
+			log.Warn().Interface("Default config", config).Msg("")
+			return config, nil
+		}
 		return nil, err
 	}
 	defer file.Close()
 
 	// unmashal the data
-	err = json.NewDecoder(file).Decode(&config)
+	err = json.NewDecoder(file).Decode(config)
 	if err != nil {
 		log.Err(err).Msg("")
 		return nil, err
 	}
 
-	return &config, nil
+	// if credentials are the default ones, warn the user
+	if config.Username == "admin" && config.Password == "admin" {
+		log.Warn().Msg("Using default credentials. Please change them in the config file.")
+	}
+
+	return config, nil
+}
+
+func getDefaultConfig() *Config {
+	return &Config{
+		Username: "admin",
+		Password: "admin",
+		Port:     "6969",
+	}
 }
